e384d7bd2be495dbbfe18095d4f334be3c115313
Mike Zhang <mzhang6@gmail.com>
2014-08-05 11:33:38 -0400

    Finished web page

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,18 @@ var gulp = require('gulp');
 var jade = require('gulp-jade');
 
 gulp.task('bower', function() {
-  gulp.src('./templates/*.jade')
-      .pipe(wiredep())
-      .pipe(gulp.dest('./templates/'));
+  gulp.src('./templates/layouts/*.jade')
+      .pipe(wiredep({
+        ignorePath: '../../build/'
+      }))
+      .pipe(gulp.dest('./templates/layouts'));
 });
 
 gulp.task('jade', function() {
   gulp.src('./templates/*.jade')
-      .pipe(jade())
+      .pipe(jade({
+        pretty: true
+      }))
       .pipe(gulp.dest('./build'));
 });
 
